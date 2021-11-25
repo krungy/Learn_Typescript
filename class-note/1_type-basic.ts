7cd1ccd0c5cb9248b480c854c790977e37c9063c
krungy <furler711@gmail.com>
2021-11-25 13:34:07 +0900

    Create project

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// JS 문자열 선언
+let str_js = 'hello'
+
+// TS 문자열 선언
+let str: string = 'hello'
+
+// TS 숫자 선언
+let num: number = 10
+
+// TS 베열 선언
+let arr: Array<number> = [1, 2, 3]
+let heroes: Array<string> = ['Captain', 'Thor', 'Hulk']
+let items: number[] = [1, 2, 3]
+
+// TS 튜플
+let address1: [string, number] = ['gangnam', 100]
+
+// TS 객체
+let obj: object = {}
+// let person: object = {
+//   name: 'cap',
+//   age: 100
+// }
+let person: { name: string, age: number } = {
+  name: 'cap',
+  age: 100
+}
+
+// TS 진위값
+let bool: boolean = true
